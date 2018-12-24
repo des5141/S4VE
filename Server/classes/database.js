719e2ef2f0bb2334111d48090e7070fe8096eb2a
Wonho ha <haho013039@gmail.com>
2018-12-25 01:03:47 +0900

    Update database.js

--- Server/classes/database.js
+++ Server/classes/database.js
@@ -9,27 +9,66 @@ const connection = mysql.createConnection({
     database: 'Heroes',
     insecureAuth: true
 });
-connection.connect();
 async function login(id, password) {
     const hashed_pass = hash.makeHash(password);
-    const sql = `select * from Users where id='${id} and password='${hashed_pass}'`;
+    const sql = `select * from Users where id='${id}' and password='${hashed_pass}'`;
     var result = false;
-    console.log(result);
-    new Promise((chain, rej) => {
+    await new Promise((chain, rej) => {
         connection.query(sql, (err, results) => {
-            console.log('asd' + results);
+            console.log('asd : ' + results);
             if (err) {
                 chain(false);
             }
-            else {
+            if (results.length === 0) {
+                chain(false);
+            }
+            if (results.length > 0) {
                 chain(true);
             }
         });
     }).then((data) => {
+        console.log(data);
         result = data;
-    })
+    });
+    return result;
+};
+async function register(id, password, name) {
+    const pass = hash.makeHash(password);
+    const point = 0;
+    const sql = `insert into Users values('${id}','${name}','${pass}','${point}')`;
+    var result = false;
+    await new Promise((a, b) => {
+        connection.query(sql, (err) => {
+            if (err) {
+                result = false;
+            }
+            result = true;
+            a();
+        });
+    });
     return result;
 };
 
-console.log(await login());
+const win_game = point =>
+    async id => {
+        const sql = `update Users set point=Users.point+${point} where id='${id}'`;
+        await new Promise((a, b) => {
+            connection.query(sql, (err) => {
+                if (err) {
+                    result = false;
+                }
+                result = true;
+                a();
+            });
+        });
+        return result;
+    };
+
+function end_connection() {
+    connection.end();
+}
+
+
+module.exports.register = register;
+module.exports.raise_point = win_game(10);
 module.exports.login = login;
